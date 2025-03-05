de80bc30b5f4ae4a780a2c73f63edd26984f78d1
Shubham Kumawat <114406903+Shubham-kumawat@users.noreply.github.com>
2025-03-05 11:57:17 +0530

    student result updated

--- 27-02-2025/namePattern/pattern.js
+++ 27-02-2025/namePattern/pattern.js
@@ -1,33 +1,36 @@
-let n = document.getElementById("number").addEventListener("input", function () {
-  n = this.value;
-});
+// let n = document.getElementById("number").addEventListener("input", function () {
+//   n = this.value;
+// });
 
-function printStars(n) {
-  if (n <= 0) {
-    return "Please enter a positive integer greater than 0.";
-  }
+// function printStars(n) {
+//   if (n <= 0) {
+//     return "Please enter a positive integer greater than 0.";
+//   }
 
-  let result = "";
-  for (let i = 1; i <= 5; i++) {
-    let str = "";
-    for (let j = 1; j <= 5; j++) {
-        if(i==1 || i==3 || i==5||(i == 2 && j == 1) || (i == 4 && j == 5)){
-      str += "* ";
-      }else{
-         str += "  ";
-      }
-    }
-    console.log(str);
-    result += str + "<br>";
-  }
-  return result;
-}
+//   let result = "";
+//   for (let i = 1; i <= n; i++) {
+//     let str = "";
+//     for (let j = 1; j <= n; j++) {
+//       // if(i == 1 || i == Math.ceil(n / 2) || i == n || (i > 1 && j == 1) || (i == n - 1 && j == n)){
+//         if(i==1 || i==3 || i==n||(i ==2 && j == 1) || (i == n-1 && j == n)){
+//       str += "* ";
+//       }else{
+//          str += "  ";
+//       }
+//     }
+//     console.log(str);
+//     result += str + "<br>";
+//   }
+//   return result;
+// }
 
-document.getElementById("submit").addEventListener("click", function () {
-  document.getElementById("result").innerHTML = printStars(n);
-});
+// document.getElementById("submit").addEventListener("click", function () {
+//   document.getElementById("result").innerHTML = printStars(n);
+// });
 
-document.getElementById("clear").addEventListener("click", function () {
-  document.getElementById("number").value = "";
-  document.getElementById("result").innerHTML = "";
-});
+// document.getElementById("clear").addEventListener("click", function () {
+//   document.getElementById("number").value = "";
+//   document.getElementById("result").innerHTML = "";
+// });
+
+
